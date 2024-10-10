03e46f30b4f4149aaf8fe6c3eaa4f9077f278c97
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2024-10-10 21:25:08 +0800

    Added Cookie Management

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import DashboardPage from "./pages/dasboard"; // Make sure the path is correct
 import "./App.css";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
+import Cookies from "js-cookie";
 
 const App = () => {
   // const { data: sessions, isLoading, error } = useGetSession();
   // const data = sessions?.data;
 
   const [sessionData, setSessionData] = React.useState({
-    employeeid: localStorage.getItem("employeeid") || "",
-    department: localStorage.getItem("department") || "",
+    employeeid: "",
+    department: "",
     date: "",
   });
 
@@ -19,13 +20,20 @@ const App = () => {
     try {
       const response = await fetch("/api/session/getsession"); // Ensure the endpoint is correct
       const data = await response.json();
-      const { employeeid, department,date } = data.data; // Ensure the structure is correct
+      const { date } = data.data; // Ensure the structure is correct
 
-      setSessionData({
-        employeeid,
-        department,
-        date,
-      });
+      // sessionData.employeeid = Cookies.get("employeeid");
+      // sessionData.department = Cookies.get("department");
+      // sessionData.date = date;
+
+      setSessionData((prevData) => ({
+        ...prevData,
+        employeeid: Cookies.get("employeeid"),
+        department: Cookies.get("department"),
+        date: date,
+      }));
+
+      // console.log(sessionData);
     } catch (error) {
       console.error("Error fetching session data:", error);
     }
